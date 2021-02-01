9b20c7086a1d87a7eee1ffd02fa4aaa6c1c1e258
devgoduck <devgoduck@gmail.com>
2021-02-01 16:14:33 +0300

    feat: completed brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const brackets = bracketsConfig.flatMap((item) => item.join(""));
+
+    for (let index = 0; index < brackets.length; index++) {
+        if (str.includes(brackets[index])) {
+            str = str.replace(brackets[index], "");
+            index = -1;
+        }
+    }
+
+    return str === "";
+};
+
+// function check(str, bracketsConfig) {
+//     const brackets = bracketsConfig.flatMap((item) => item.join(""));
+
+//   for (let index = 0; index < brackets.length; index++) {
+//     if (str.includes(brackets[index])) {
+//         str = str.replace(brackets[index], "");
+//         index = -1;
+//     }
+// }
+
+// return str === "";
+// }
+
+// console.log(check("123", [1, 2, 3, [4, 6, [4, 5, 4]]]));
